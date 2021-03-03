34e7a98c37550ceaff3f55181f6ebc789deb4503
Debasish <Debasishghosh444x@gmail.com>
2021-03-03 10:06:32 +0530

    3/3/2021 : 10:06

---
+++ Git.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Git extends Controller
+{
+
+    function index()
+    {
+        echo "This is 1nd commit";
+        echo "This is 2nd commit";
+        echo "this is the 3rd change";
+
+    }
+
+}
---
+++ Git2.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Git2 extends Controller
+{
+    function index()
+    {
+        echo "This is 2nd commit";
+    }
+}
